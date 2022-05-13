a6750f150ca3ddcd11ca18bdd8aaa594cc6e7432
Faceoff <faceoff@localhost.localdomain>
2022-05-13 13:20:51 +0300

    new name

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ export const projects = [
     link: "https://alexzvigun-travel-advisor.netlify.app",
   },
   {
-    title: "DevChat",
+    title: "Crypto Outlook",
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
